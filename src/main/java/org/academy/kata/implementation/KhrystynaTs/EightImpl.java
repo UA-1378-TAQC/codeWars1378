2df9759f00c400d5c9e070b4dac1215298b80303
Khrystyna <kristina50963@gmail.com>
2025-04-01 22:12:12 +0300

    implementation done

--- src/main/java/org/academy/kata/implementation/KhrystynaTs/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/KhrystynaTs/EightImpl.java
@@ -29,16 +29,16 @@ public class EightImpl extends Base implements IEight {
         if (input ==null || input.length == 0){
             return new int[]{};
         }
-        int[] myArray = {0,0};
+        int[] array = {0,0};
         for (int j : input) {
             if (j > 0) {
-                myArray[0]++;
+                array[0]++;
             }
             if (j <= 0) {
-                myArray[1] += j;
+                array[1] += j;
             }
         }
-        return myArray;
+        return array;
     }
 
     @Override
