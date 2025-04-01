e77f7e4792a2140408424acc1542115bafea5558
Khrystyna <kristina50963@gmail.com>
2025-04-01 20:10:29 +0300

    adding implementation to task if (input ==null || input.length == 0){

--- src/main/java/org/academy/kata/implementation/KhrystynaTs/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/KhrystynaTs/EightImpl.java
@@ -26,7 +26,19 @@ public class EightImpl extends Base implements IEight {
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if (input ==null || input.length == 0){
+            return new int[]{};
+        }
+        int[] myArray = {0,0};
+        for (int j : input) {
+            if (j > 0) {
+                myArray[0]++;
+            }
+            if (j <= 0) {
+                myArray[1] += j;
+            }
+        }
+        return myArray;
     }
 
     @Override
