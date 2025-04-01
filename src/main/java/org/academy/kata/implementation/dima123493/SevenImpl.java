e6d81d2629ca7e4e085aa60d843ddcd779f0804e
Dmytro Zubenko <fokist@outlook.com>
2025-04-01 18:08:40 +0300

    Looking for a benefactor task has been done

--- src/main/java/org/academy/kata/implementation/dima123493/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/dima123493/SevenImpl.java
@@ -3,10 +3,20 @@ package org.academy.kata.implementation.dima123493;
 import org.academy.kata.Base;
 import org.academy.kata.ISeven;
 
+import java.util.Arrays;
+
 public class SevenImpl extends Base implements ISeven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = Arrays.stream(arr).sum();
+        int n = arr.length;
+        double requiredDonation = navg * (n + 1) - sum;
+
+        if (requiredDonation <= 0) {
+            throw new IllegalArgumentException("Donation cannot be less or equal to 0");
+        }
+
+        return (long) Math.ceil(requiredDonation);
     }
 
     @Override
