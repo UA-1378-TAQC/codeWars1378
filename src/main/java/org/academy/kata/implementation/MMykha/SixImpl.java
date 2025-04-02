8b555fe6ce7430092c03849f68c5301042ca7107
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-04-02 18:56:59 +0300

    Implemented solution for build a pile of cubes task

--- src/main/java/org/academy/kata/implementation/MMykha/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/MMykha/SixImpl.java
@@ -2,11 +2,22 @@ package org.academy.kata.implementation.MMykha;
 
 import org.academy.kata.Base;
 import org.academy.kata.ISix;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class SixImpl extends Base implements ISix {
     @Override
     public long findNb(long m) {
-        return 0;
+        BigDecimal bigN = BigDecimal.valueOf(m)
+                .sqrt(MathContext.DECIMAL128)
+                .multiply(BigDecimal.valueOf(8))
+                .add(BigDecimal.ONE)
+                .sqrt(MathContext.DECIMAL128)
+                .subtract(BigDecimal.ONE)
+                .divide(BigDecimal.valueOf(2), RoundingMode.HALF_UP);
+        if(bigN.remainder(BigDecimal.ONE).equals(BigDecimal.ZERO) && bigN.signum()==1) return bigN.longValue();
+        else return -1;
     }
 
     @Override
