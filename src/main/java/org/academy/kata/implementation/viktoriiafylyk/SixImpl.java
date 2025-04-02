de842a50380294c124c0c5d47350db9a8938abbb
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-02 20:36:01 +0300

    Implementing balance method

--- src/main/java/org/academy/kata/implementation/viktoriiafylyk/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/SixImpl.java
@@ -2,6 +2,8 @@ package org.academy.kata.implementation.viktoriiafylyk;
 
 import org.academy.kata.Base;
 import org.academy.kata.ISix;
+import java.util.Arrays;
+
 
 public class SixImpl extends Base implements ISix {
     @Override
@@ -11,7 +13,27 @@ public class SixImpl extends Base implements ISix {
 
     @Override
     public String balance(String book) {
-        return "";
+        String[] arr = book
+                .replaceAll("[^a-zA-Z0-9\n. ]", "").split("\n+");
+
+        StringBuilder sb = new StringBuilder();
+        double balance = Double.parseDouble(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            balance -= Double.parseDouble(arr[i].split("\\s+")[2]);
+            sb.append(String.format("%s Balance %.2f\\r\\n", arr[i].trim().replaceAll("\\s+", " "), balance));
+        }
+
+        return String.format("Original Balance: %s\\r\\n", arr[0])
+                + sb.toString()
+                + String.format("Total expense  %.2f\\r\\n"
+                , Arrays.stream(arr, 1, arr.length)
+                        .mapToDouble(a -> Double.parseDouble(a.split("\\s+")[2]))
+                        .sum())
+                + String.format("Average expense  %.2f"
+                , Arrays.stream(arr, 1, arr.length)
+                        .mapToDouble(a -> Double.parseDouble(a.split("\\s+")[2]))
+                        .average()
+                        .getAsDouble());
     }
 
     @Override
