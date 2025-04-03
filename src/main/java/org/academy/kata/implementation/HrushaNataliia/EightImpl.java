3168b6d5eb016aeb7efcb7067bcc42b655629516
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-03 10:58:29 +0300

    added implementation to Volume of a Cuboid  task

--- src/main/java/org/academy/kata/implementation/HrushaNataliia/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/HrushaNataliia/EightImpl.java
@@ -6,12 +6,12 @@ import org.academy.kata.IEight;
 public class EightImpl extends Base implements IEight {
     @Override
     public int liters(double time) {
-        return (int)(time * 0.5);
+        return 0;
     }
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+        return length * width * height;
     }
 
     @Override
