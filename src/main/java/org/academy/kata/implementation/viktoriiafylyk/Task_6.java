1bb78f578f66d39e30b4c13a49833d991d4e4e0b
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-05 19:27:51 +0300

    added implementation of Task 6 Convert a String to a Number!

---
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_6.java
@@ -0,0 +1,21 @@
+package org.academy.kata.implementation.viktoriiafylyk;
+import org.academy.kata.IEight;
+import org.academy.util.Authors;
+import org.academy.util.data.IReader;
+import org.academy.util.data.IWriter;
+
+public class Task_6 {
+    private final IEight impl;
+    private String str;
+
+    public Task_6(IEight impl) {
+        this.impl = impl;
+    }
+
+    public void run(Authors author, IReader reader, IWriter writer) {;
+        String input = reader.readString("Введіть рядок, який містить число :");
+        int result = impl.stringToNumber(input);
+        writer.writePrompt("Результат конвертування : ");
+        writer.writeResult(result);
+    }
+}
