b4d57c975906fbdfceb61e1653486ef6e365e894
adidvar <adidvar@gmail.com>
2025-04-05 14:21:24 +0300

    Fix of interface and new folder for tasks

---
+++ src/main/java/org/academy/tasks/Task1.java
@@ -0,0 +1,13 @@
+package org.academy.tasks;
+
+import org.academy.interfaces.ITask;
+import org.academy.util.Authors;
+import org.academy.util.data.IReader;
+import org.academy.util.data.IWriter;
+
+public class Task1 implements ITask {
+    @Override
+    public void run(Authors author, IReader reader, IWriter writer) {
+
+    }
+}
