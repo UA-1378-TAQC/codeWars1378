25b831c0f819092cde229d1a30c3bce45eab6ff3
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-05 19:33:56 +0300

    added implementation of Task 7 Wilson Primes

---
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_7.java
@@ -0,0 +1,22 @@
+package org.academy.kata.implementation.viktoriiafylyk;
+import org.academy.kata.IEight;
+import org.academy.util.Authors;
+import org.academy.util.data.IReader;
+import org.academy.util.data.IWriter;
+public class Task_7  {
+    private final IEight impl;
+    private long n;
+
+
+    public Task_7(IEight impl) {
+        this.impl = impl;
+    }
+
+    public void run(Authors author, IReader reader, IWriter writer) {
+        long number = reader.readLong("Введіть число для форматування :");
+        boolean isWilson = impl.am_i_wilson(number);
+        writer.writePrompt("Результат : ");
+        writer.writeResult(isWilson);
+    }
+
+}
