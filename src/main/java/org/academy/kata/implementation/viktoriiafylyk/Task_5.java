39b5b1aa134ba2768c47a5aa14359579ccbc4804
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-05 19:16:14 +0300

    added implementation of Task 5 (Count of positives/sum of negatives)

---
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_5.java
@@ -0,0 +1,22 @@
+package org.academy.kata.implementation.viktoriiafylyk;
+import org.academy.kata.IEight;
+import org.academy.util.Authors;
+import org.academy.util.data.IReader;
+import org.academy.util.data.IWriter;
+
+
+public class Task_5 {
+    private final IEight impl;
+    private int[] input;
+     public Task_5(IEight impl) {
+         this.impl = impl;
+     }
+
+     public void run(Authors author, IReader reader, IWriter writer) {
+         int[] input = reader.readIntArray("Введіть масив цілих чисел (додатні та від'ємні) :", "" );
+         int[] result = impl.countPositivesSumNegatives(input);
+         writer.writePrompt("Результат : ");
+         writer.writeResult(result);
+     }
+
+}
