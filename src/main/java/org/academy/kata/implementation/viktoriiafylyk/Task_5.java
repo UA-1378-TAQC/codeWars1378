b6b65e2774b8afe0be5ce1a5fb5969cb3e81c69d
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-05 20:55:44 +0300

    updated

--- src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_5.java
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_5.java
@@ -1,11 +1,13 @@
+
 package org.academy.kata.implementation.viktoriiafylyk;
 import org.academy.kata.IEight;
 import org.academy.util.Authors;
 import org.academy.util.data.IReader;
 import org.academy.util.data.IWriter;
+import org.academy.interfaces.ITask;
 
 
-public class Task_5 {
+public class Task_5 implements ITask{
     private final IEight impl;
     private int[] input;
      public Task_5(IEight impl) {
