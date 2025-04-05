ef94b0ec787ef68754c4016e9d929456134270e4
adidvar <adidvar@gmail.com>
2025-04-05 23:36:21 +0300

    Architecture added

--- src/main/java/org/academy/tasks/Task1.java
+++ src/main/java/org/academy/tasks/Task1.java
@@ -8,6 +8,18 @@ import org.academy.util.data.IWriter;
 public class Task1 implements ITask {
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
+        /*
+        writer.writePrompt("Введіть n");
+        int value1 = reader.readInt(); //41234123 [1,100];
+        writer.writePrompt("Введіть k");
+        int value2 = reader.readInt(); //324124231
 
+        //validate
+
+        int result = author.getFive().solve(value1,value2);
+
+        writer.writePrompt("Результат:");
+        writer.writeResult(result);
+         */
     }
 }
