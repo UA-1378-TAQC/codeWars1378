807c26721c15a1359dafdb61e5d74e5032c1c558
Dmytro Zubenko <fokist@outlook.com>
2025-04-06 17:10:59 +0300

    writer commented out

--- src/main/java/org/academy/kata/implementation/dima123493/Task13.java
+++ src/main/java/org/academy/kata/implementation/dima123493/Task13.java
@@ -8,7 +8,7 @@ import org.academy.util.data.IWriter;
 public class Task13 implements ITask {
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
-        writer.writePrompt("Enter book values: ");
+        //writer.writePrompt("Enter book values: ");
 
         String numberWithDecimal = "\\d+\\.\\d{2}";// Number format with a period and two digits after
         String newline = "\\n";// Line separator
@@ -24,7 +24,7 @@ public class Task13 implements ITask {
 
         String result = author.getSix().balance(book);
 
-        writer.writePrompt("Result:");
-        writer.writeResult(result);
+        //writer.writePrompt("Result:");
+        //writer.writeResult(result);
     }
 }
