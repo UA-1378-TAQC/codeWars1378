98d6930c76a5d57a3c22a0bc950423e5f618301d
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-04-06 03:43:36 +0300

    Implemented solution for find the smallest task

--- src/main/java/org/academy/kata/implementation/MMykha/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/MMykha/FiveImpl.java
@@ -28,6 +28,26 @@ public class FiveImpl extends Base implements IFive {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String minNumberString = Long.toString(n);
+        long min = n;
+        int indexI = -1;
+        int indexJ = -1;
+        for(int i = 0; i<minNumberString.length(); i++){
+            char removedDigit = minNumberString.charAt(i);
+            StringBuilder t = new StringBuilder(minNumberString);
+            t.deleteCharAt(i);
+            for(int j = 0; j<minNumberString.length(); j++){
+                if(i==j) continue;
+                StringBuilder newT = new StringBuilder(t);
+                newT.insert(j, removedDigit);
+                long check = Long.parseLong(newT.toString());
+                if(check<min){
+                    min = check;
+                    indexI=i;
+                    indexJ=j;
+                }
+            }
+        }
+        return new long [] {min,indexI,indexJ};
     }
 }
