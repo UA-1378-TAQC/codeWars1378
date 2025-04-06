9580ecc2f9c1bd3806661777af5dabf9eb319175
BohdanKovalets <kovaletss.bogdan@gmail.com>
2025-04-06 16:12:57 +0300

    TaskManager finished

--- src/main/java/org/academy/TaskManager.java
+++ src/main/java/org/academy/TaskManager.java
@@ -1,9 +1,15 @@
 package org.academy;
 
 import org.academy.util.Authors;
+import org.academy.util.Tasks;
+import org.academy.util.data.ConsoleReader;
+import org.academy.util.data.IReader;
+import org.academy.util.data.IWriter;
 
 public class TaskManager {
-    public void run(Authors authors, int id){
-        
+    public void run(Authors authors, Tasks tasks){
+        IReader reader = new ConsoleReader();
+        IWriter writer = new ConsoleWriter();
+        tasks.getRunner().run(authors, reader, writer);
     }
 }
