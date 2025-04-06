af882e22ccda5a6a51f7bd428353c01f3fa27169
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-04-06 21:44:53 +0300

    Implementation of validation of results

--- src/main/java/org/academy/tasks/Task9.java
+++ src/main/java/org/academy/tasks/Task9.java
@@ -9,13 +9,17 @@ public class Task9 implements ITask {
     public Task9() {}
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
-        writer.writePrompt("Для виконання цього завдання необхідно ввести масив чилел та дільник:");
-        writer.writePrompt("1.Введіть масив додатніх цілих чисел:");
+        writer.writePrompt("Для виконання цього завдання необхідно ввести список чисел та дільник.");
+        writer.writePrompt("Введіть список додатніх цілих чисел:");
         int [] numbers = reader.readIntArray(0);
-        writer.writerPrompt("Введіть дільник (має бути цілим додатнім числом, більше нуля):");
+        writer.writePrompt("Введіть дільник (має бути цілим додатнім числом):");
         int divider = reader.readInt(1);
-        int [] result = author.getEight().divisibleBy(numbers,divider);
-        writer.writePrompt("Результат виконання - всі введені числа, які діляться на введений дільник:");
-        writer.writeIntArray(result);
+        try {
+            int[] result = author.getEight().divisibleBy(numbers, divider);
+            writer.writePrompt("Результат виконання - всі введені числа, які діляться на введений дільник:");
+            writer.writeIntArray(result);
+        }catch(Exception e){
+            writer.writePrompt("Щось пішло не так.");
+        }
     }
 }
