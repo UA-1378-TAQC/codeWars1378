5d65adadc71b44aecbe4a36423c3f24f04a72a57
Dmytro Zubenko <fokist@outlook.com>
2025-04-07 01:52:39 +0300

    string checker added

--- src/main/java/org/academy/tasks/Task13.java
+++ src/main/java/org/academy/tasks/Task13.java
@@ -11,18 +11,17 @@ public class Task13 implements ITask {
         writer.writePrompt("Enter book values: ");
 
         String numberWithDecimal = "\\d+\\.\\d{2}";// Number format with a period and two digits after
-        String newline = "\\n";// Line separator
+        String newline = "\\R";// Line separator
         String checkNumber = "\\d+";// Check number
         String category = "[A-Za-z0-9]+";// Category (one word, no spaces)
         String amount = "\\d+\\.\\d{2}";// Amount (same as the initial balance)
-
 
         String entryLine = newline + checkNumber + " " + category + " " + amount;
         String fullRegex = "^" + numberWithDecimal + "(" + entryLine + ")*$";
 
         String book = reader.readString(fullRegex);
 
-        if (book.isEmpty()) {
+        if (book == null || book.isBlank()) {
             writer.writePrompt("Check book string cannot be empty");
             return;
         }
