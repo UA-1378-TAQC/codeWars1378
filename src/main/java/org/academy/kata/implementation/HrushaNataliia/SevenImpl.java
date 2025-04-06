081cc60e41746529e7027b11782c67c904ae1382
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-06 11:56:26 +0300

    added implementation to Looking for a benefactor task

--- src/main/java/org/academy/kata/implementation/HrushaNataliia/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/HrushaNataliia/SevenImpl.java
@@ -6,7 +6,19 @@ import org.academy.kata.ISeven;
 public class SevenImpl extends Base implements ISeven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0.0;
+        for (double num : arr) {
+            sum += num;
+        }
+        int n = arr.length;
+        double x = navg * (n + 1) - sum;
+
+        if (x <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        long result = (long) Math.ceil(x);
+        return result;
     }
 
     @Override
