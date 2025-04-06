9ded7045c376f363e30b1c996f6f7164f4409e2d
Dmytro Zubenko <fokist@outlook.com>
2025-04-07 01:42:11 +0300

    book string checker added

--- src/main/java/org/academy/tasks/Task13.java
+++ src/main/java/org/academy/tasks/Task13.java
@@ -8,7 +8,7 @@ import org.academy.util.data.IWriter;
 public class Task13 implements ITask {
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
-        //writer.writePrompt("Enter book values: ");
+        writer.writePrompt("Enter book values: ");
 
         String numberWithDecimal = "\\d+\\.\\d{2}";// Number format with a period and two digits after
         String newline = "\\n";// Line separator
@@ -22,9 +22,14 @@ public class Task13 implements ITask {
 
         String book = reader.readString(fullRegex);
 
+        if (book.isEmpty()) {
+            writer.writePrompt("Check book string cannot be empty");
+            return;
+        }
+
         String result = author.getSix().balance(book);
 
-        //writer.writePrompt("Result:");
-        //writer.writeResult(result);
+        writer.writePrompt("Result:");
+        writer.writeResult(result);
     }
 }
