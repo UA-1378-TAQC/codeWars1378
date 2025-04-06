d6ab486474faec7fb47197df55944dc555339b5a
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-06 13:38:09 +0300

    updated

--- src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_8.java
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_8.java
@@ -14,6 +14,7 @@ public class Task_8 implements ITask {
     public Task_8(IEight impl) {
         this.impl = impl;
     }
+    @Override
     public void run(Authors author, IReader reader, IWriter writer) {
         double number = reader.readDouble("Введіть число для форматування :");
         double result = impl.TwoDecimalPlaces(number);
