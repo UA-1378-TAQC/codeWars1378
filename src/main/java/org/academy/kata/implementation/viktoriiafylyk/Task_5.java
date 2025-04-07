29581c9bd726103317c7e130c27f891642824caf
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-07 09:19:57 +0300

    updated

--- src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_5.java
+++ src/main/java/org/academy/kata/implementation/viktoriiafylyk/Task_5.java
@@ -1,22 +1,18 @@
 
 package org.academy.kata.implementation.viktoriiafylyk;
-import org.academy.kata.IEight;
 import org.academy.util.Authors;
 import org.academy.util.data.IReader;
 import org.academy.util.data.IWriter;
 import org.academy.interfaces.ITask;
 
 public class Task_5 implements ITask{
-    private final IEight impl;
-     public Task_5(IEight impl) {
-         this.impl = impl;
-     }
 
     @Override
      public void run(Authors author, IReader reader, IWriter writer) {
+         writer.writePrompt("Введіть масив цілих чисел (додатні та від'ємні) :");
          Integer minValue = 0 ;
          int[] input = reader.readIntArray(minValue);
-         int[] result = impl.countPositivesSumNegatives(input);
+         int[] result = author.getEight().countPositivesSumNegatives(input);
          writer.writePrompt("Результат : ");
          writer.writeResult(result);
      }
