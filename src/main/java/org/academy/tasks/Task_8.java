9fddc32f6559084251f4f6d89b7e8da8acb03eb1
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-07 09:47:32 +0300

    updated

--- src/main/java/org/academy/tasks/Task_8.java
+++ src/main/java/org/academy/tasks/Task_8.java
@@ -1,23 +1,17 @@
-package org.academy.kata.implementation.viktoriiafylyk;
-
+package org.academy.tasks;
 import org.academy.interfaces.ITask;
-import org.academy.kata.IEight;
 import org.academy.util.Authors;
 import org.academy.util.data.IReader;
 import org.academy.util.data.IWriter;
 
 public class Task_8 implements ITask {
-    private final IEight impl;
-    private double number;
 
-
-    public Task_8(IEight impl) {
-        this.impl = impl;
-    }
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
-        double number = reader.readDouble("Введіть число для форматування :");
-        double result = impl.TwoDecimalPlaces(number);
+        Double minValue = 0.0;
+        writer.writePrompt("Введіть число для форматування :");
+        double number = reader.readDouble(minValue);
+        double result = author.getEight().TwoDecimalPlaces(number);
         writer.writePrompt("Результат форматування : ");
         writer.writeResult(result);
     }
