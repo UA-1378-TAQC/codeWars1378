afc510a4265b96aae77c2d3ced182371f23f0a6d
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-07 09:35:04 +0300

    updated  :)

--- src/main/java/org/academy/tasks/Task_6.java
+++ src/main/java/org/academy/tasks/Task_6.java
@@ -1,22 +1,16 @@
-package org.academy.kata.implementation.viktoriiafylyk;
+package org.academy.tasks;
 import org.academy.interfaces.ITask;
-import org.academy.kata.IEight;
 import org.academy.util.Authors;
 import org.academy.util.data.IReader;
 import org.academy.util.data.IWriter;
 
 public class Task_6 implements ITask {
-    private final IEight impl;
-    private String str;
-
-    public Task_6(IEight impl) {
-        this.impl = impl;
-    }
     @Override
-
     public void run(Authors author, IReader reader, IWriter writer) {;
-        String input = reader.readString("Введіть рядок, який містить число :");
-        int result = impl.stringToNumber(input);
+        String integerRegex = "^[-+]?\\d+$";
+        writer.writePrompt("Введіть рядок, який містить число : ");
+        String input = reader.readString(integerRegex);
+        int result = author.getEight().stringToNumber(input);
         writer.writePrompt("Результат конвертування : ");
         writer.writeResult(result);
     }
