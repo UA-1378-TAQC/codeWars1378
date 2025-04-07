555e25e56057dd4d6107b0b03ef2e1bb80b17dd9
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-07 13:26:14 +0300

    task 20 Perimeter of squares in a rectangle

---
+++ src/main/java/org/academy/tasks/Task20.java
@@ -0,0 +1,21 @@
+package org.academy.tasks;
+
+import org.academy.interfaces.ITask;
+import org.academy.util.Authors;
+import org.academy.util.data.IReader;
+import org.academy.util.data.IWriter;
+
+import java.math.BigInteger;
+
+public class Task20 implements ITask {
+    @Override
+    public void run(Authors author, IReader reader, IWriter writer) {
+        writer.writePrompt("Введіть число n для обчислення периметра (n ≥ 0):");
+        BigInteger n = reader.readBigInteger(BigInteger.ZERO);
+
+        BigInteger result = author.getFive().perimeter(n);
+
+        writer.writePrompt("Периметр для n = " + n + " дорівнює:");
+        writer.writeResult(result);
+    }
+}
