383f5c7e90b575de29ec8484c6d64ab4f5ddeedd
MarryMik <57491436+MarryMik@users.noreply.github.com>
2025-04-07 12:38:51 +0300

    in Task18, Task9, and Task5 in run() the methods for printing their results were changed

--- src/main/java/org/academy/tasks/Task9.java
+++ src/main/java/org/academy/tasks/Task9.java
@@ -17,7 +17,7 @@ public class Task9 implements ITask {
         try {
             int[] result = author.getEight().divisibleBy(numbers, divider);
             writer.writePrompt("Результат. Всі введені числа, які діляться на введений дільник:");
-            writer.writeResult(result);
+            writer.writeArray(result);
         }catch(Exception e){
             writer.writePrompt("Щось пішло не так.");
         }
