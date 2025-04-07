f6f9abe79477d4330bb3be306048bd0938c77244
Наталія <138515375+HrushaNataliia@users.noreply.github.com>
2025-04-07 15:46:54 +0300

    translated writePrompt to english

--- src/main/java/org/academy/tasks/Task20.java
+++ src/main/java/org/academy/tasks/Task20.java
@@ -11,19 +11,19 @@ import java.math.BigInteger;
 public class Task20 implements ITask {
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
-        writer.writePrompt("Введіть число n для обчислення периметра (n ≥ 0):");
+        writer.writePrompt("Enter number n to calculate perimeter (n ≥ 0):");
 
         String input = reader.readString(null);
 
         if (!InputValidator.isValidBigInteger(input, BigInteger.ZERO)) {
-            writer.writePrompt("Помилка: введено некоректне число. Число має бути цілим і ≥ 0");
+            writer.writePrompt("Error: Invalid input. Number must be an integer ≥ 0");
             return;
         }
 
         BigInteger n = new BigInteger(input);
 
         BigInteger result = author.getFive().perimeter(n);
-        writer.writePrompt("Периметр для n = " + n + " дорівнює:");
+        writer.writePrompt("Perimeter for n = " + n + " equals:");
         writer.writeResult(result);
     }
 }
