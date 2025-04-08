1e6351feff81142e91ab33c0fa202d9769f429f7
Dmytro Zubenko <fokist@outlook.com>
2025-04-08 15:22:38 +0300

    Class rename and magic number correction

--- src/main/java/org/academy/tasks/taskFloatingPointApproximation.java
+++ src/main/java/org/academy/tasks/taskFloatingPointApproximation.java
@@ -4,11 +4,12 @@ import org.academy.util.Authors;
 import org.academy.util.data.IReader;
 import org.academy.util.data.IWriter;
 
-public class TaskF implements ITask {
+public class taskFloatingPointApproximation implements ITask {
     @Override
     public void run(Authors author, IReader reader, IWriter writer) {
         writer.writePrompt("Enter x value: ");
-        double x = reader.readDouble(0.0);
+        double minimumValue = 0.0;
+        double x = reader.readDouble(minimumValue);
 
         double result = author.getSix().f(x);
 
