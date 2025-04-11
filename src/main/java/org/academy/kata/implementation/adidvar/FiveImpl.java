728a126ed4da7337813ab993b08e19e5a55e725a
adidvar <adidvar@gmail.com>
2025-04-11 15:21:46 +0300

    zeros()

--- src/main/java/org/academy/kata/implementation/adidvar/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/adidvar/FiveImpl.java
@@ -26,8 +26,40 @@ public class FiveImpl extends Base implements IFive {
         return 0;
     }
 
+    private static String moveCharacter(String str, int fromIndex, int toIndex) {
+        char charToMove = str.charAt(fromIndex);
+        StringBuilder sb = new StringBuilder(str);
+        sb.deleteCharAt(fromIndex);
+
+        sb.insert(toIndex, charToMove);
+
+        return sb.toString();
+    }
+
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        int count = String.valueOf(n).length();
+
+        long min = n;
+        int r1 = -1,r2 = -1;
+
+        for(int i = 0 ; i < count; i++){
+            for(int j = 0 ; j < count; j++){
+                if(i==j) continue;
+
+                String string = String.valueOf(n);
+                string = moveCharacter(string,i,j);
+
+                long new_num = Long.parseLong(string);
+
+                if(new_num < min){
+                    min = new_num;
+                    r1 = i;
+                    r2 = j;
+                }
+            }
+        }
+
+        return new long[] {min,(long)r1,(long)r2};
     }
 }
