fba1541d7270487f79df122e3c3f25315adc03bb
adidvar <adidvar@gmail.com>
2025-04-11 15:54:51 +0300

    added code

--- src/main/java/org/academy/kata/implementation/adidvar/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/adidvar/SevenImpl.java
@@ -11,6 +11,12 @@ public class SevenImpl extends Base implements ISeven {
 
     @Override
     public String seriesSum(int n) {
-        return "";
+        if(n==0)
+            return "0.00";
+        double number = 1;
+        for(int i = 1 ; i < n ; i++){
+            number += 1.0/(3*i+1);
+        }
+        return String.format("%.02f", number);
     }
 }
