d1ac3fdd04e356d48c1cf098ad24f89d19b82810
BohdanKovalets <kovaletss.bogdan@gmail.com>
2025-04-12 02:10:01 +0300

    task Find-the-smallest done

--- src/main/java/org/academy/kata/implementation/BohdanKovalets/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/BohdanKovalets/FiveImpl.java
@@ -28,6 +28,33 @@ public class FiveImpl extends Base implements IFive {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String numberStr = Long.toString(n);
+        int length = numberStr.length();
+
+        long minResult = n;
+        int from = 0;
+        int to = 0;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                StringBuilder temp = new StringBuilder(numberStr);
+                char currentDigit = temp.charAt(i);
+                temp.deleteCharAt(i);
+                temp.insert(j, currentDigit);
+
+                long candidate = Long.parseLong(temp.toString());
+
+                boolean better = candidate < minResult;
+                boolean sameButEarlier = candidate == minResult && (i < from || (i == from && j < to));
+
+                if (better || sameButEarlier) {
+                    minResult = candidate;
+                    from = i;
+                    to = j;
+                }
+            }
+        }
+
+        return new long[] { minResult, from, to };
     }
 }
