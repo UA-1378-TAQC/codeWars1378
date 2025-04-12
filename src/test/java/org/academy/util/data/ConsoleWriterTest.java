48dff5ee2cc6b1542d58ac1131fbb083b5a124c1
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-12 13:45:58 +0300

    Implmented testTestWriteResult2 for double

--- src/test/java/org/academy/util/data/ConsoleWriterTest.java
+++ src/test/java/org/academy/util/data/ConsoleWriterTest.java
@@ -1,9 +1,29 @@
 package org.academy.util.data;
 
+import org.academy.kata.console.ConsoleOutputCaptor;
+import org.academy.kata.dataproviders.WriterDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class ConsoleWriterTest {
+
+    private ConsoleOutputCaptor captor;
+    private IWriter writer;
+
+    @BeforeMethod
+    public void captureInput() {
+        writer = new ConsoleWriter();
+        captor = new ConsoleOutputCaptor();
+        captor.startCapture();
+    }
+
+    @AfterMethod
+    public void releaseCapture() {
+        captor.stopCapture();
+    }
 
     @Test
     public void testWritePrompt() {
@@ -21,8 +41,13 @@ public class ConsoleWriterTest {
     public void testTestWriteResult1() {
     }
 
-    @Test
-    public void testTestWriteResult2() {
+    @Test(dataProvider = "doubleDataProvider", dataProviderClass = WriterDataProvider.class)
+    public void testTestWriteResult2(double value, String expectedOutput) {
+        writer.writeResult(value);
+        String actualOutput = captor.getOutput().trim();
+
+        Assert.assertEquals(actualOutput, expectedOutput,
+                "Expected output: " + expectedOutput + ", but got: " + actualOutput);
     }
 
     @Test
