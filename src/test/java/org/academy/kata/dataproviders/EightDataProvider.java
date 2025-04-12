ae065320c2b51cb96d508f00ecdf06ecb9c1190b
adidvar <adidvar@gmail.com>
2025-04-12 16:21:18 +0300

    Tests for stringToNumber()

--- src/test/java/org/academy/kata/dataproviders/EightDataProvider.java
+++ src/test/java/org/academy/kata/dataproviders/EightDataProvider.java
@@ -2,6 +2,7 @@ package org.academy.kata.dataproviders;
 
 import org.academy.kata.IEight;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.util.List;
 
@@ -32,6 +33,20 @@ public class EightDataProvider extends BaseDataProvider{
                 {0.0, 0.0, 0.0, 0.0},
                 {-1.0, -2.0, -3.0, -6.0},
                 {1.5, 2.5, -3.5, -13.125},
+        };
+        return combineDataProviders(data, EIGHTS);
+    }
+
+    @DataProvider(name="stringToNumberDataProvider")
+    protected Object[][] stringToNumberDataProvider(){
+        Object[][] data =  new Object[][]{
+                {"1234", 1234},
+                {"605", 605},
+                {"1405", 1405},
+                {"-7", -7},
+                {"-2147483648", -2147483648},
+                {"2147483647", 2147483647},
+                {"0", 0},
         };
         return combineDataProviders(data, EIGHTS);
     }
