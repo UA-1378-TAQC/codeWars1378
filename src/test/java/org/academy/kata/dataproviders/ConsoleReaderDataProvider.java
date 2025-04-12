ede5465cbb7e409f0a3abf4ef0c85aff3db81623
Viktoriis Fylyk <vfilik194@gmail.com>
2025-04-12 16:15:54 +0300

    Implemented testReadLong() from ConsolReader

---
+++ src/test/java/org/academy/kata/dataproviders/ConsoleReaderDataProvider.java
@@ -0,0 +1,18 @@
+package org.academy.kata.dataproviders;
+
+import org.testng.annotations.DataProvider;
+
+public class ConsoleReaderDataProvider {
+
+    @DataProvider(name = "longDataProvider")
+    public Object[][] longDataProvider() {
+        return new Object[][]{
+                {0L, "0", 0L},
+                {1L, "1", 1L},
+                {-1L, "-1", -1L},
+                {100L, "100", 100L},
+                {Long.MAX_VALUE, String.valueOf(Long.MAX_VALUE), Long.MAX_VALUE},
+                {Long.MIN_VALUE, String.valueOf(Long.MIN_VALUE), Long.MIN_VALUE}
+        };
+    }
+}
--- src/test/java/org/academy/util/data/ConsoleReaderTest.java
+++ src/test/java/org/academy/util/data/ConsoleReaderTest.java
@@ -1,16 +1,38 @@
 package org.academy.util.data;
 
+import org.academy.kata.console.ConsoleInputCaptor;
+import org.academy.kata.dataproviders.ConsoleReaderDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class ConsoleReaderTest {
+    private ConsoleInputCaptor inputCaptor;
+    private IReader reader;
+
+    @BeforeMethod
+    public void setUpInputStream() {
+        inputCaptor = new ConsoleInputCaptor();
+        reader = new ConsoleReader();
+    }
+
+    @AfterMethod
+    public void restoreInputStream() {
+        inputCaptor.restoreInput();
+    }
 
     @Test
     public void testReadInt() {
     }
 
-    @Test
-    public void testReadLong() {
+    @Test(dataProvider = "longDataProvider", dataProviderClass = ConsoleReaderDataProvider.class)
+    public void testReadLong(long minValue, String simulatedInput, long expectedResult) {
+        inputCaptor.setInput(simulatedInput);
+        long actualResult = reader.readLong(minValue);
+        Assert.assertEquals(actualResult, expectedResult,
+                String.format("Expected: %d, but got: %d", expectedResult, actualResult));
     }
 
     @Test
