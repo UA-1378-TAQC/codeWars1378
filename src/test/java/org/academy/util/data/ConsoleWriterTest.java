69418903aa09c3ec3d96ea1472a853430c80eb3d
Anatolii <you@example.com>
2025-04-13 16:16:12 +0300

    Added tests to cover method, which outputs the Object to the console

--- src/test/java/org/academy/util/data/ConsoleWriterTest.java
+++ src/test/java/org/academy/util/data/ConsoleWriterTest.java
@@ -1,9 +1,26 @@
 package org.academy.util.data;
 
+import org.academy.kata.console.ConsoleOutputCaptor;
+import org.academy.kata.dataproviders.ConsoleWriterDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-public class ConsoleWriterTest {
+public class ConsoleWriterTest extends ConsoleWriterDataProvider {
+    private ConsoleOutputCaptor captor;
+
+    @BeforeMethod
+    public void captureInput() {
+        captor = new ConsoleOutputCaptor();
+        captor.startCapture();
+    }
+
+    @AfterMethod
+    public void releaseCapture() {
+        captor.stopCapture();
+    }
 
     @Test
     public void testWritePrompt() {
@@ -37,8 +54,11 @@ public class ConsoleWriterTest {
     public void testTestWriteResult5() {
     }
 
-    @Test
-    public void testTestWriteResult6() {
+    @Test(dataProvider = "writeResultObjectDataProvider")
+    public void testTestWriteResultObject(Object input, String expected) {
+        ConsoleWriter consoleWriter = new ConsoleWriter();
+        consoleWriter.writeResult(input);
+        Assert.assertEquals(expected, captor.getOutput());
     }
 
     @Test
