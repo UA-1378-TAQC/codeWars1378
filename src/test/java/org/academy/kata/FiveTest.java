c0cbe9fc4ab064c44db81499e7c5d5c305dc13e4
novitskiiy <veremienko.bogdan@gmail.com>
2025-04-14 12:42:37 +0300

    testZeros

--- src/test/java/org/academy/kata/FiveTest.java
+++ src/test/java/org/academy/kata/FiveTest.java
@@ -1,6 +1,9 @@
     package org.academy.kata;
 
+    import org.academy.kata.dataproviders.FiveDataProvider;
     import org.testng.annotations.Test;
+
+    import static org.testng.Assert.assertEquals;
 
     public class FiveTest {
 
@@ -8,8 +11,10 @@
         public void testGap() {
         }
 
-        @Test
-        public void testZeros() {
+        @Test(dataProvider = "zerosDataProvider", dataProviderClass = FiveDataProvider.class)
+        public void testZeros(IFive impl, int input, int expected) {
+            int result = impl.zeros(input);
+            assertEquals(result, expected, "Failed for input: " + input);
         }
 
         @Test
--- src/test/java/org/academy/kata/dataproviders/FiveDataProvider.java
+++ src/test/java/org/academy/kata/dataproviders/FiveDataProvider.java
@@ -2,6 +2,8 @@ package org.academy.kata.dataproviders;
 
 
 import org.academy.kata.IFive;
+import org.academy.kata.implementation.novitskiiy.FiveImpl;
+import org.testng.annotations.DataProvider;
 
 import java.util.List;
 
@@ -22,4 +24,19 @@ public class FiveDataProvider extends BaseDataProvider {
             new org.academy.kata.implementation.viktoriiafylyk.FiveImpl()
 
     );
+
+    @DataProvider(name = "zerosDataProvider")
+    public Object[][] provideZerosData() {
+        return new Object[][]{
+                {new FiveImpl(), 0, 0},
+                {new FiveImpl(), 5, 1},
+                {new FiveImpl(), 6, 1},
+                {new FiveImpl(), 10, 2},
+                {new FiveImpl(), 25, 6},
+                {new FiveImpl(), 100, 24},
+                {new FiveImpl(), 125, 31},
+                {new FiveImpl(), 200, 49},
+                {new FiveImpl(), 1000, 249}
+        };
+    }
 }
