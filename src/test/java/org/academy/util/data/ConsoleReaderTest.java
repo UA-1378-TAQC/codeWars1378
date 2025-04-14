6679c10a1bcf1723e55acb212a299cbd24c42b98
adidvar <adidvar@gmail.com>
2025-04-14 09:53:58 +0300

    added tests

--- src/test/java/org/academy/util/data/ConsoleReaderTest.java
+++ src/test/java/org/academy/util/data/ConsoleReaderTest.java
@@ -30,18 +30,6 @@ public class ConsoleReaderTest {
 
     @Test
     public void testReadBigInteger() {
-        String input = "5\n15\n";
-        InputStream testInput = new ByteArrayInputStream(input.getBytes());
-        InputStream originalIn = System.in;
-
-        try {
-            System.setIn(testInput); // Перекидаємо ввід
-            ConsoleReader reader = new ConsoleReader(); // Буде використовувати наш testInput
-            BigInteger result = reader.readBigInteger(BigInteger.TEN);
-            assertEquals(result, BigInteger.valueOf(15));
-        } finally {
-            System.setIn(originalIn); // Обов'язково відновити
-        }
     }
 
     @Test
