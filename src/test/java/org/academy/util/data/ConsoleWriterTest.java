871e8fd80775f1794ec0f5a64f3656d1170070c3
BohdanKovalets <kovaletss.bogdan@gmail.com>
2025-04-14 11:03:34 +0300

    added testwriteresult-string

--- src/test/java/org/academy/util/data/ConsoleWriterTest.java
+++ src/test/java/org/academy/util/data/ConsoleWriterTest.java
@@ -1,16 +1,38 @@
 package org.academy.util.data;
 
+import org.academy.kata.console.ConsoleOutputCaptor;
+import org.academy.kata.dataproviders.WriterStringDataProvider;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 
 public class ConsoleWriterTest {
+    private ConsoleOutputCaptor captor;
+    private IWriter writer;
+
+    @BeforeMethod
+    public void captureInput() {
+        writer = new ConsoleWriter();
+        captor = new ConsoleOutputCaptor();
+        captor.startCapture();
+    }
+
+    @AfterMethod
+    public void releaseCapture() {
+        captor.stopCapture();
+    }
 
     @Test
     public void testWritePrompt() {
     }
 
-    @Test
-    public void testWriteResult() {
+    @Test(dataProvider = "stringData", dataProviderClass = WriterStringDataProvider.class)
+    public void testWriteResult(String input, String expectedOutput) {
+        writer.writeResult(input);
+        assertEquals(captor.getOutput(), expectedOutput);
     }
 
     @Test
