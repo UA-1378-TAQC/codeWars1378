100a2cc4755aca05778b449fb7343fc509c493a4
Liubomyr Halamaha <galamagal86@gmail.com>
2025-04-15 11:48:04 +0300

    fix

--- src/test/java/org/academy/kata/dataproviders/WriterDataProvider.java
+++ src/test/java/org/academy/kata/dataproviders/WriterDataProvider.java
@@ -2,6 +2,9 @@ package org.academy.kata.dataproviders;
 
 import org.testng.annotations.DataProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WriterDataProvider {
 
     @DataProvider(name = "characterData")
@@ -113,5 +116,22 @@ public class WriterDataProvider {
                 {new double[]{0.0}, "0.0"},
                 {new double[]{Double.NaN}, "NaN"},
         };
+    }
+
+    @DataProvider(name = "writeResultObjectDataProvider")
+    protected Object[][] writeResultObjectDataProvider(){
+        Object[][] data = new Object[][]{
+                {101, "101"},
+                {3.99999, "3.99999"},
+                {3.99999f, "3.99999"},
+                {47L, "47"},
+                {'c', "c"},
+                {"%", "%"},
+                {"hello", "hello"},
+                {false, "false"},
+                {List.of(1, 2, 3), "[1, 2, 3]"},
+                {new ArrayList<>(), "[]"}
+        };
+        return data;
     }
 }
--- src/test/java/org/academy/util/data/ConsoleWriterTest.java
+++ src/test/java/org/academy/util/data/ConsoleWriterTest.java
@@ -1,7 +1,6 @@
 package org.academy.util.data;
 
 import org.academy.kata.console.ConsoleOutputCaptor;
-import org.academy.kata.dataproviders.ConsoleWriterDataProvider;
 import org.academy.kata.dataproviders.WriterDataProvider;
 import org.academy.kata.dataproviders.WriterStringDataProvider;
 import org.testng.Assert;
@@ -9,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ConsoleWriterTest extends WriterDataProvider , ConsoleWriterDataProvider {
+public class ConsoleWriterTest extends WriterDataProvider {
     private ConsoleOutputCaptor captor;
     private IWriter writer;
 
@@ -61,8 +60,8 @@ public class ConsoleWriterTest extends WriterDataProvider , ConsoleWriterDataPro
 
         writer.writeResult(input);
 
-        String output = outputCaptor.getOutput();
-        assertEquals(output.trim(), expectedOutput.trim(), "Output mismatch for input: " + input);
+        String output = captor.getOutput();
+        Assert.assertEquals(output.trim(), expectedOutput.trim(), "Output mismatch for input: " + input);
     }
 
     @Test(dataProvider = "booleanDataProvider", dataProviderClass = WriterDataProvider.class)
