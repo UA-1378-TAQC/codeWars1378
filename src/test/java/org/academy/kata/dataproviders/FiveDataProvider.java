8f4fff2dffe4b5df3dae64329186cd68690680b5
Dmytro Slobodianiuk <bristlbeak205@gmail.com>
2025-04-15 17:24:05 +0300

    fix

--- src/test/java/org/academy/kata/dataproviders/FiveDataProvider.java
+++ src/test/java/org/academy/kata/dataproviders/FiveDataProvider.java
@@ -1,25 +1,25 @@
 package org.academy.kata.dataproviders;
 
-import org.academy.kata.IFive;
-import org.testng.annotations.DataProvider;
-
 import java.math.BigInteger;
 import java.util.List;
 
+import org.academy.kata.IFive;
+import org.testng.annotations.DataProvider;
+
 public class FiveDataProvider extends BaseDataProvider {
     private final static List<IFive> FIVES = List.of(
-            new org.academy.kata.implementation.adidvar.FiveImpl(),
-            new org.academy.kata.implementation.Anat0li1.FiveImpl(),
-            new org.academy.kata.implementation.BohdanKovalets.FiveImpl(),
-            new org.academy.kata.implementation.dima123493.FiveImpl(),
-            new org.academy.kata.implementation.DMSlobodianiuk.FiveImpl(),
-            new org.academy.kata.implementation.HrushaNataliia.FiveImpl(),
-            new org.academy.kata.implementation.KhrystynaTs.FiveImpl(),
-            new org.academy.kata.implementation.MMykha.FiveImpl(),
-            new org.academy.kata.implementation.novitskiiy.FiveImpl(),
-            new org.academy.kata.implementation.roman50021.FiveImpl(),
-            new org.academy.kata.implementation.RomanKmet.FiveImpl(),
-            new org.academy.kata.implementation.viktoriiafylyk.FiveImpl()
+            // new org.academy.kata.implementation.adidvar.FiveImpl(),
+            // new org.academy.kata.implementation.Anat0li1.FiveImpl(),
+            // new org.academy.kata.implementation.BohdanKovalets.FiveImpl(),
+            // new org.academy.kata.implementation.dima123493.FiveImpl(),
+            new org.academy.kata.implementation.DMSlobodianiuk.FiveImpl()
+            // new org.academy.kata.implementation.HrushaNataliia.FiveImpl(),
+            // new org.academy.kata.implementation.KhrystynaTs.FiveImpl(),
+            // new org.academy.kata.implementation.MMykha.FiveImpl(),
+            // new org.academy.kata.implementation.novitskiiy.FiveImpl(),
+            // new org.academy.kata.implementation.roman50021.FiveImpl(),
+            // new org.academy.kata.implementation.RomanKmet.FiveImpl(),
+            // new org.academy.kata.implementation.viktoriiafylyk.FiveImpl()
 
     );
 
