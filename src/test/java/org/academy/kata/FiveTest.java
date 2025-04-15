34ed520a6de54e422138c7cefc737b662e1652da
Khrystyna <kristina50963@gmail.com>
2025-04-15 10:01:18 +0300

    add soft assert

--- src/test/java/org/academy/kata/FiveTest.java
+++ src/test/java/org/academy/kata/FiveTest.java
@@ -2,10 +2,12 @@
 
     import org.academy.kata.dataproviders.FiveDataProvider;
     import org.testng.annotations.Test;
+    import org.testng.asserts.SoftAssert;
+
     import static org.testng.Assert.assertEquals;
 
     public class FiveTest extends FiveDataProvider {
-
+        SoftAssert softAssert = new SoftAssert();
         @Test
         public void testGap() {
         }
@@ -25,10 +27,10 @@
         @Test(dataProvider = "smallestValueDataProvider")
         public void testSmallest(long input, long[] expected, IFive iFive) {
             long[] actual = iFive.smallest(input);
-            assertEquals(actual.length, expected.length, "Length mismatch");
+            softAssert.assertEquals(actual.length, expected.length, "Length mismatch");
 
             for (int i = 0; i < expected.length; i++) {
-                assertEquals(actual[i], expected[i], "Mismatch at index " + i);
+                softAssert.assertEquals(actual[i], expected[i], "Mismatch at index " + i);
             }
         }
     }
